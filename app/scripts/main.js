5aca258623941d83bf3b46838d3c5f685f9cb63a
Artur Pawlak <pawlak.artur90@gmail.com>
2016-04-04 19:42:50 +0200

    Update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,15 +6,29 @@ $(document).ready(function() {
     changeQuantity(6);
     addEvents();
 
+    var Numbers = {
+        element : [],
+        setNumbers: function (arg, quantity) {
+            this.element = [];
+            for(var i = 0; i < quantity; i++){
+                this.element[i] = arg[i].innerHTML;
+            }
+        },
+        getNumbers: function () {
+            return this.element;
+        }
+    };
+
     //funckja dezaktywujaca radio buttony
     function deactivationRadioButtons(arg){
         var radio = document.getElementsByName("sort");
+        var radioLength = radio.length;
         if(arg){
-            for(var i = 0; i < radio.length; i++){
+            for(var i = 0; i < radioLength; i++){
                 radio[i].setAttribute("disabled", "disabled");
             }
         }else {
-            for(var i = 0; i < radio.length; i++){
+            for(var i = 0; i < radioLength; i++){
                 radio[i].removeAttribute('disabled');
             }
         }
@@ -23,12 +37,13 @@ $(document).ready(function() {
     //funkcja dezaktywujaca start buttony
     function deactivationStartButtons(arg){
         var start = document.getElementsByClassName("start-button");
+        var startLength = start.length;
         if(arg){
-            for(var i = 0; i < start.length; i++){
+            for(var i = 0; i < startLength; i++){
                 start[i].setAttribute("disabled", "disabled");
             }
         }else {
-            for(var i = 0; i < start.length; i++){
+            for(var i = 0; i < startLength; i++){
                 start[i].removeAttribute('disabled');
             }
         }
@@ -37,10 +52,16 @@ $(document).ready(function() {
     //funkcja dezaktywujaca random button
     function deactiveRandomButton(arg){
         var rand = document.getElementById("random");
+        var add = document.getElementById("add");
+        var string = document.getElementById("string");
         if(arg){
             rand.setAttribute("disabled", "disabled");
+            add.setAttribute("disabled", "disabled");
+            string.setAttribute("disabled", "disabled");
         }else {
             rand.removeAttribute('disabled');
+            add.removeAttribute('disabled');
+            string.removeAttribute('disabled');
         }
     }
 
@@ -77,12 +98,30 @@ $(document).ready(function() {
         quantityDiv.innerHTML = quantity;
     }
 
-    //dodawanie losowych liczb do elementow
-    function randomElements(arg){
+    //dodawanie losowych liczb jezeli arg === true, dodawanie wpisanych liczb jezeli arg === false
+    function addElements(arg, quantity){
         var element = document.getElementsByClassName("elements");
-        for(var i = 0; i < arg; i++){
-            element[i].innerText = Math.floor((Math.random() * 10) + 1);
+        if(arg){
+            for(var i = 0; i < quantity; i++){
+                element[i].innerText = Math.floor((Math.random() * 15) + 1);
+            }
+            Numbers.setNumbers(element, quantity);
+        } else{
+            var string = document.getElementById("string");
+            var stringArray = string.value.split(" ");
+            var stringLenth = stringArray.length;
+            var elementLenth = quantity;
+            if(stringLenth == elementLenth){
+                for(var i = 0; i < quantity; i++){
+                    element[i].innerText = stringArray[i];
+                }
+                Numbers.setNumbers(element, quantity);
+                deactivationRadioButtons(false);
+            } else{
+                alert("Nieprawidłowa ilość elementów!");
+            }
         }
+        getElements();
     }
 
     //dodawanie eventow do elementow
@@ -93,6 +132,7 @@ $(document).ready(function() {
             changeQuantity(slider.value);
             changeElements(14 - slider.value);
             deactivationRadioButtons(true);
+            deactivationStartButtons(true);
             deactiveRandomButton(false);
         });
         //event on slider for Chrome, FF, Opera
@@ -105,18 +145,257 @@ $(document).ready(function() {
         var random = document.getElementById("random");
         //event on random button
         random.addEventListener("click", function(){
-            randomElements(slider.value);
+            // randomElements(slider.value);
+            addElements(true, slider.value);
             deactivationRadioButtons(false);
         });
+        var add = document.getElementById("add");
+        //event on add button
+        add.addEventListener("click", function(){
+            addElements(false, slider.value);
+        });
         var radio = document.getElementsByName("sort");
+        var radioLength = radio.length;
         //events for radio buttons
-        for(var i = 0; i < radio.length; i++){
+        for(var i = 0; i < radioLength; i++){
             (function(e){
                 radio[e].addEventListener("click", function(){
-                console.log(radio[e].value);
-                deactiveRandomButton(true);
-            });
+                    choose(radio[e].value);
+                    deactiveRandomButton(true);
+                    deactivationStartButtons(false);
+                });
             })(i);
         }
+    }
+
+    ///////////////////////////////////////////////////
+    //function for test////////////////////////////////
+    ///////////////////////////////////////////////////
+    function getElements(){
+        var numbers = [];
+        var numbersLength = Numbers.getNumbers().length;
+        for(var i = 0; i < numbersLength; i++){
+            numbers[i] = parseInt(Numbers.getNumbers()[i]);
+        }
+        return numbers;
+    }
+
+    //funkcja wybierajaca radio button
+    function choose(arg) {
+        var elements = getElements();
+        var elementsLength = elements.length;
+        switch (arg) {
+            case "bubble":
+                bubbleSort(elements, elementsLength);
+                break;
+            case "quick":
+                addBefore(elements);
+                quickSort(elements, elementsLength, 0, elementsLength - 1);
+                break;
+            case "insertion":
+                insertionSort(elements, elementsLength);
+                break;
+            case "selection":
+                selectionSort(elements, elementsLength);
+                break;
+            case "counting":
+                countingSort(elements, elementsLength);
+                break;
+            case "radix":
+                radixSort(elements, elementsLength);
+                break;
+            case "heap":
+                heapSort(elements, elementsLength);
+                break;
+        }
+    }
+
+    function addBefore(arg) {
+        var before = document.getElementById("result-before");
+        before.innerHTML = "";
+        var numbersLength = arg.length;
+        for(var i = 0; i < numbersLength; i++){
+            before.innerHTML += arg[i] + " ";
+        }
+    }
+
+    function addAfter(arg) {
+        var after = document.getElementById("result-after");
+        after.innerHTML = "";
+        var numbersLength = arg.length;
+        for(var i = 0; i < numbersLength; i++){
+            after.innerHTML += arg[i] + " ";
+        }
+    }
+
+    //funkcja sortujaca algorytmem babelkowym
+    function bubbleSort(arg, n) {
+        var numbers = [], i,
+        tmp = 0,
+        p = 0;
+        for(i = 0; i < n; i++){
+            numbers[i] = arg[i];
+        }
+        addBefore(numbers);
+        for(var j = n -1; j > 0; j--){
+            p = 1;
+            for(var i = 0; i < j; i++){
+                if(numbers[i] > numbers[i + 1]){
+                    tmp = numbers[i];
+                    numbers[i] = numbers[i + 1]
+                    numbers[i + 1] = tmp;
+                    p = 0;
+                }
+            }
+            if(p){
+                break;
+            }
+        }
+        addAfter(numbers);
+    }
+
+    function quickSort(arg, n, left, right) {
+        // do zrobienia
+        var numbers = [],
+        i, j, tmp, piwot;
+        for(i = 0; i < n; i++){
+            numbers[i] = arg[i];
+        }
+        i = Math.floor((left + right) / 2);
+        piwot = numbers[i];
+        numbers[i] = numbers[right];
+        for(j = i = left; i < right; i++){
+            if(numbers[i] < piwot){
+                tmp = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = tmp;
+                j++;
+            }
+        }
+        numbers[right] = numbers[j];
+        numbers[j] = piwot;
+        if(left < j - 1){
+            quickSort(numbers, n, left, j - 1);
+        }else if(j + 1 < right){
+            quickSort(numbers, n, j + 1, right);
+        } else {
+            addAfter(numbers);
+        }
+    }
+
+    function insertionSort(arg, n) {
+        var numbers = [],
+        i, j, tmp;
+        for(i = 0; i < n; i++){
+            numbers[i] = arg[i];
+        }
+        addBefore(numbers);
+        for(j = n -2; j >= 0; j--){
+            tmp = numbers[j];
+            i = j + 1;
+            while((i < n) && (tmp > numbers[i])){
+                numbers[i - 1] = numbers[i];
+                i++;
+            }
+            numbers[i - 1] = tmp;
+        }
+        addAfter(numbers);
+    }
+
+    function selectionSort(arg, n) {
+        var numbers = [],
+        i, j, min, tmp;
+        for(i = 0; i < n; i++){
+            numbers[i] = arg[i];
+        }
+        addBefore(numbers);
+        for(j = 0; j < n - 1; j++){
+            min = j;
+            for(i = j + 1; i < n; i++){
+                if(numbers[i] < numbers[min]){
+                    min = i;
+                }
+            }
+            tmp = numbers[min];
+            numbers[min] = numbers[j];
+            numbers[j] = tmp;
+        }
+        addAfter(numbers);
+    }
+
+    function countingSort(arg, n) {
+        var numbers = [];
+        var min = 1, max = 15;
+        var i, z = 0, count = [];
+        for(i = 0; i < n; i++){
+            numbers[i] = arg[i];
+        }
+        addBefore(numbers);
+         for (i = min; i <= max; i++) {
+            count[i] = 0;
+        }
+        for (i=0; i < numbers.length; i++) {
+            count[numbers[i]]++;
+        }
+        for (i = min; i <= max; i++) {
+            while (count[i]-- > 0) {
+                numbers[z++] = i;
+            }
+        }
+        addAfter(numbers);
+    }
+
+    function radixSort(arg, n) {
+        // do zrobienia
+        var numbers = [], i;
+        for(i = 0; i < n; i++){
+            numbers[i] = arg[i];
+        }
+        addBefore(numbers);
+        addAfter(numbers);
+    }
+
+    function heapSort(arg, n) {
+        // do zrobienia
+        var numbers = [],
+        i, j, k, m, tmp;
+        for(i = 0; i < n; i++){
+            numbers[i] = arg[i];
+        }
+        addBefore(numbers);
+        for(i = 2; i <= n; i++){
+            j = i;
+            k = Math.floor(j / 2);
+            tmp = numbers[i];
+            while((k > 0) && (numbers[k] < tmp)){
+                numbers[j] = numbers[k];
+                j = k;
+                k = Math.floor(j / 2);
+            }
+            numbers[j] = tmp;
+        }
+        for(i = n; i > 1; i--){
+            tmp = numbers[1];
+            numbers[1] = numbers[i];
+            numbers[i] = tmp;
+            j = 1;
+            k = 2;
+            while(k < i){
+                if((k + 1 < i) && (numbers[k + 1] > numbers[k])){
+                    m = k + 1;
+                }else {
+                    m = k;
+                }
+                if(numbers[m] <= numbers[j]){
+                    break;
+                }
+                tmp = numbers[j];
+                numbers[j] = numbers[m];
+                numbers[m] = tmp;
+                j = m;
+                k = j + j;
+            }
+        }
+        addAfter(numbers);
     }
 });
